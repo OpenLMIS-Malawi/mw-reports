3389a8cbaaaf12a3e407aa00b4f66d0eac4a152b
Jakub Kondrat <jkondrat@soldevelo.com>
2018-01-16 15:46:18 +0100

    MW-701: Add supported formats to existing reports

---
+++ src/main/resources/db/migration/20180116152616932__update_reports_add_suported_formats.sql
@@ -0,0 +1,6 @@
+-- Pick Work Sheet
+INSERT INTO jaspertemplate_supportedformats (jaspertemplateid, supportedformats) VALUES ('afbd56e8-bc66-446a-a947-810971f68aef', 'csv');
+INSERT INTO jaspertemplate_supportedformats (jaspertemplateid, supportedformats) VALUES ('afbd56e8-bc66-446a-a947-810971f68aef', 'xls');
+-- Regular vs Emergency Orders
+INSERT INTO jaspertemplate_supportedformats (jaspertemplateid, supportedformats) VALUES ('e734ee66-2d14-4f33-99e7-b7a8407e3e39', 'csv');
+INSERT INTO jaspertemplate_supportedformats (jaspertemplateid, supportedformats) VALUES ('e734ee66-2d14-4f33-99e7-b7a8407e3e39', 'xls');
