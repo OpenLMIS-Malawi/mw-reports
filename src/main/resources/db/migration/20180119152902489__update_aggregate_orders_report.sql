287512017816de54425037a83759a4ad4caeda54
Jakub Kondrat <jkondrat@soldevelo.com>
2018-01-19 15:43:55 +0100

    MW-746: Update aggregate orders report

---
+++ src/main/resources/db/migration/20180119152902489__update_aggregate_orders_report.sql
@@ -0,0 +1,5 @@
+-- Aggregate Orders
+UPDATE jasper_templates SET type = 'Aggregate Orders Report' WHERE id = 'f28d0ebd-7276-4453-bc3c-48556a4bd25a';
+
+-- Aggregate Orders - XLS/CSV
+UPDATE jasper_templates SET type = 'Aggregate Orders Report' WHERE id = '5e378334-d1fe-4915-902e-22ecd0a61f5b';
