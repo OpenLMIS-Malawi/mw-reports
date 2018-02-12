9bdc9efc7196c4960e665c39363d3693f4f69867
Jakub Kondrat <jkondrat@soldevelo.com>
2018-02-12 17:19:05 +0100

    MALAWISUP-445: Use xlsx instead of xls in Pick Work Sheet report

---
+++ src/main/resources/db/migration/20180212170733241__update_pick_work_sheet.sql
@@ -0,0 +1,3 @@
+-- Pick Work Sheet
+DELETE FROM jaspertemplate_supportedformats WHERE jaspertemplateid = 'afbd56e8-bc66-446a-a947-810971f68aef' AND supportedformats = 'xls';
+INSERT INTO jaspertemplate_supportedformats (jaspertemplateid, supportedformats) VALUES ('afbd56e8-bc66-446a-a947-810971f68aef', 'xlsx');
