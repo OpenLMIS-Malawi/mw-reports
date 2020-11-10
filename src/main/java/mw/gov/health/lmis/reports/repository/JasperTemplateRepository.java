c1c4684668a7a7767aae3432d1f68ac09ac2408c
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2020-11-10 12:01:38 +0100

    MALAWISUP-2354: Fixed displaying jasper templates

--- src/main/java/mw/gov/health/lmis/reports/repository/JasperTemplateRepository.java
+++ src/main/java/mw/gov/health/lmis/reports/repository/JasperTemplateRepository.java
@@ -3,6 +3,7 @@ package mw.gov.health.lmis.reports.repository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 import mw.gov.health.lmis.reports.domain.JasperTemplate;
@@ -10,4 +11,6 @@ import mw.gov.health.lmis.reports.domain.JasperTemplate;
 public interface JasperTemplateRepository
     extends PagingAndSortingRepository<JasperTemplate, UUID> {
   JasperTemplate findByName(@Param("name") String name);
+
+  List<JasperTemplate> findByIsDisplayed(@Param("isDisplayed") boolean isDisplayed);
 }
