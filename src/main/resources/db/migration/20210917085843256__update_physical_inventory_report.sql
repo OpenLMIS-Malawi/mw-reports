d8fb0003b2f775af57c5e8af88008c5e51764cc0
MagdaToczek <kocik.magdalena@gmail.com>
2021-09-17 11:09:06 +0200

    MW-1060: Modified flag display for physical inventory report

---
+++ src/main/resources/db/migration/20210917085843256__update_physical_inventory_report.sql
@@ -0,0 +1,5 @@
+--
+-- Data for Name: jasper_templates; Type: TABLE DATA; Schema: reports; Owner: postgres
+--
+
+UPDATE reports.jasper_templates SET isdisplayed = false WHERE id = '6bf325d4-0755-453c-b606-573dd455ab09';
