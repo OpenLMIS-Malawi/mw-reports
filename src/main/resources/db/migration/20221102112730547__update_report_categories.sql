faf49c427ebaea414d6fc73e69417b3db7c3d55f
anawrotsoldevelo <anawrot@soldevelo.com>
2022-11-02 13:12:16 +0100

    Updated report categories

---
+++ src/main/resources/db/migration/20221102112730547__update_report_categories.sql
@@ -0,0 +1,8 @@
+UPDATE reports.jasper_templates
+SET category='Aditional Reports'
+WHERE name='Reporting Rate Report'
+OR name='EPI Stock on Hand by Batch by Facility'
+OR name='EPI Product Issues Report by Batch'
+OR name='EPI Aggregate Products Issued and Received Per Facility Report'
+OR name='EPI Product Adjustment Report'
+OR name='EPI Aggregate Report Summary';
