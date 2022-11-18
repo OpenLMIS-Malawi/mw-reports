7ab1ce284e437e95ccced862a50890bdeaef565e
anawrotsoldevelo <anawrot@soldevelo.com>
2022-11-18 13:24:49 +0100

    MALAWISUP-4181: Updated old reporting rate report

---
+++ src/main/resources/db/migration/20221118121727792__update_old_reporting_rate_report.sql
@@ -0,0 +1,7 @@
+UPDATE template_parameters
+SET selectexpression='/api/reports/programs'
+WHERE id='551795e8-5777-49c4-8e0c-b76bd65f8aed';
+
+UPDATE template_parameters
+SET selectexpression='/api/reports/processingPeriods'
+WHERE id='1bb77e82-f27a-4069-a9da-4a86a7e4efea';
