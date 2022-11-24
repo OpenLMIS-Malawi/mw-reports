f5826f0109000862e70a15c02c0ebfab1a1520f7
Adam Nawrot <113436691+anawrotsoldevelo@users.noreply.github.com>
2022-11-24 12:37:35 +0100

    MALAWISUP-4202: Changed naming of exported reports (#38)
    * MALAWISUP-4202: Changed naming of exported reports
    
    * MALAWISUP-4202: Changed template parameter names to lowercase
    
    * MALAWISUP-4202: Fixed table name in migration

---
+++ src/main/resources/db/migration/20221124093822961__changed_template_parameter_names_to_lowercase.sql
@@ -0,0 +1,23 @@
+UPDATE reports.template_parameters
+SET name='program'
+WHERE id='55f3108a-fe41-4fce-a62a-fc0d3c9ce44e';
+
+UPDATE reports.template_parameters
+SET name='geographicZone'
+WHERE id='726a82f3-c0d6-4d92-8f87-189abde5fcee';
+
+UPDATE reports.template_parameters
+SET name='program'
+WHERE id='551795e8-5777-49c4-8e0c-b76bd65f8aed';
+
+UPDATE reports.template_parameters
+SET name='period'
+WHERE id='1bb77e82-f27a-4069-a9da-4a86a7e4efea';
+
+UPDATE reports.template_parameters
+SET name='geographicZone'
+WHERE id='25d3ddaf-ccc7-4d3a-ad07-4b168f2bb4bb';
+
+UPDATE reports.template_parameters
+SET name='dueDays'
+WHERE id='3d1c018d-92d3-4286-9743-502dedf75f3d';
